d714b037ea05710f4d9bf092562ef153b45b6c1f
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-10-27 00:04:39 +0300

    user and admin view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+@section('content')
+    <h1>admin dashboard</h1>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,10 @@ Route::get('/', [AppController::class, 'index'])->name('app.index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function(){
+    Route::get('/my-account', [UserController::class,'index'])->name('user.index');
+});
+
+Route::middleware(['auth','auth.admin'])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
