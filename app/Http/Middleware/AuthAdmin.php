ce05f16fd9b259122390f7c67d679f19f516c192
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-10-26 22:43:58 +0300

    added controllers for user and admin

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthAdmin
@@ -15,6 +16,11 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if(Auth::user()->utype != 'ADM')
+        {
+            session()->flush();
+            return redirect()->route('login');
+        }
         return $next($request);
     }
 }
