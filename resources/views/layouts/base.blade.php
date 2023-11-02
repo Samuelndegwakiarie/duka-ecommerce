4d39fefd997f23a71bfa84ab17a0cd3dc6fbe2e6
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-11-02 23:15:24 +0300

    changes

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -139,9 +139,9 @@
                                             </li>
                                             <li><a href="{{route ('app.index')}}" class="nav-link menu-title">Home</a></li>
                                             <li><a href="{{route ('shop.index')}}" class="nav-link menu-title">Shop</a></li>
-                                            <li><a href="{{route ('cart/list.index')}}" class="nav-link menu-title">Cart</a></li>
-                                            <li><a href="{{route ('about-us.index')}}" class="nav-link menu-title">About Us</a></li>
-                                            <li><a href="{{route ('contact-us.index')}}" class="nav-link menu-title">Contact Us</a>
+                                            <li><a href="cart/list.html" class="nav-link menu-title">Cart</a></li>
+                                            <li><a href="about-us.html" class="nav-link menu-title">About Us</a></li>
+                                            <li><a href="contact-us.html" class="nav-link menu-title">Contact Us</a>
                                             </li>
                                             <li><a href="{{route ('blog.index')}}" class="nav-link menu-title">Blog</a></li>
                                         </ul>
