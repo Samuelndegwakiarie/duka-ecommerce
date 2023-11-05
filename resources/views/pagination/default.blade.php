52ae59cc1a90a59bb2113ec0bc40d78331db2c20
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-11-06 00:22:50 +0300

    pagination

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -20,24 +20,51 @@
                             </a>
                         </li>
                         @endif
+
+                        @foreach ($elements as $element)
+                        @if (is_string($element))
+                            <li class="page-item">
+                            <a class="page-link diabled" href="javascript:void(0)">{{$element}}</a>
+                        </li>
+                        @endif
+
+                        @if (is_array($element))
+                        @foreach ($element as $page=>$url)
+                            @if ($page == $paginator->currentPage())
+                            <li class="page-item active">
+                                <a class="page-link" href="javascript:void(0)">{{$page}}</a>
+                            </li>
+                            @else
+                            <li class="page-item">
+                                <a class="page-link" href="{{$url}}">{{$page}}</a>
+                            </li>
+                            @endif
                             
+                        @endforeach
+                            
+                        @endif
+                            
+                        @endforeach   
                         
-
-
-                        <li class="page-item active">
-                            <a class="page-link" href="javascript:void(0)">1</a>
-                        </li>
+                        @if ($paginator->hasMorePages())
                         <li class="page-item">
-                            <a class="page-link" href="shop-1.html?page=2">2</a>
-                        </li>
-
-                        <li class="page-item">
-                            <a href="shop-1.html?page=2" class="page-link" aria-label="Next">
+                            <a href="{{paginator->nextPageUrl()}}" class="page-link" aria-label="Next">
                                 <span aria-hidden="true">
                                     <i class="fas fa-chevron-right"></i>
                                 </span>
                             </a>
                         </li>
+                            
+                        @else
+                        <li class="page-item">
+                            <a href="javascript:void(0)" class="page-link disabled" aria-label="Next">
+                                <span aria-hidden="true">
+                                    <i class="fas fa-chevron-right"></i>
+                                </span>
+                            </a>
+                        </li>
+                        @endif
+                        
 
                     </ul>
                 </nav>
