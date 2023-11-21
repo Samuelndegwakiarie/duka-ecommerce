d03ac5e765a1a6a39496b91ba07aaabfe96db0a6
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-11-21 13:58:56 +0300

    updated shopcontroller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,18 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-
 class ShopController extends Controller
 {
     public function index()
     {
-        $products = Product::orderBy('created_at','DESC')->$paginate(12);
-        return view('shop',['products'=>$products]);
+        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
+        return view('shop', ['products' => $products]);
     }
 
     public function productDetails($slug)
     {
-        $product = Product::where('slug',$slug)->first();
-        return view ('details',['product'=>$product]);
+        $product = Product::where('slug', $slug)->first();
+        return view('details', ['product' => $product]);
     }
 }
