ec8a6f226c9bf50290975240b45b76be4d63fe3b
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-11-21 14:32:05 +0300

    route for product details

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ use App\Http\Controllers\ShopController;
 
 Route::get('/', [AppController::class, 'index'])->name('app.index');
 Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
-Route::get('/product/{slug}',[ShopController::class,'productDetails'])->name('shop.product.details');
+
+Route::get('/product/details/{slug}', [ShopController::class,'productDetails'])->name('shop.product.details');
+
 
 Auth::routes();
 
